65d166d31213be7c44e06c4be2714940773e6e38
cordovatj <cordovatj@pdvsa.com>
2017-03-06 14:52:28 -0400

    Corrección de bug con subcarpetas y tópicos al generar autocompletacion agregando archivo con rutas de tópicos. Se aborta la generación de autocompletación al encontrar carpetas duplicadas. Sustituidas rutas literales por variables

--- gac.sh
+++ gac.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 
 DIRBASE=$1
+RUTA_CACHE=~/.cache/chu
+ARCHIVO_TOPICOS=$RUTA_CACHE/lista_topicos
+ARCHIVO_RUTAS_TOPICOS=$RUTA_CACHE/lista_rutas_topicos
+ARCHIVO_TOPICOS_REPETIDOS=$RUTA_CACHE/lista_topicos_repetidos
 TEMP=`mktemp /tmp/chuleta.XXXXX`
-rm ~/.cache/chu/*
-for line in $(find "$DIRBASE" -type f -iname "chuleta*.txt");do 
-	echo "$line"|xargs dirname|sed 's#.*/##' >> $TEMP
-done
-sort -u $TEMP|tr '\n' ' ' > ~/.cache/chu/lista_topicos
-rm $TEMP
+TEMP2=`mktemp /tmp/chuleta.XXXXX`
+TEMP3=`mktemp -d /tmp/chuleta.XXXXX`
 
-for line in $(cat ~/.cache/chu/lista_topicos);do
-	find "$DIRBASE/$line" -type f -iname "chuleta*.txt" \
+function borrar_temp()
+{
+	rm -rf $TEMP ${TEMP2} ${TEMP3}
+}
+
+
+for line in $(find "$DIRBASE" -type f -iname "chuleta*.txt");do
+	VA=$(echo "$line"|xargs dirname|sed 's#.*/##')
+	echo $VA >> $TEMP
+	VB=$(echo "$line"|xargs dirname)
+	echo "$VA	$VB" >> ${TEMP2}
+done
+cp $RUTA_CACHE/* ${TEMP3}/
+rm $RUTA_CACHE/*
+sort -u $TEMP|tr '\n' ' ' > $ARCHIVO_TOPICOS
+sort -u ${TEMP2} > $ARCHIVO_RUTAS_TOPICOS
+
+if [ $(cat $ARCHIVO_RUTAS_TOPICOS |cut -f 1|uniq -c|grep -vn "1"|wc -l) -gt 0 ];then
+	echo
+	echo Tópicos duplicados. No se puede actualizar datos
+	echo de autocompletación:
+	echo
+	x="$(cat $ARCHIVO_RUTAS_TOPICOS |cut -f 1|uniq -c|grep -v "1"|awk '{print $2}')"
+	find $DIRBASE -type d -iname "$x"
+	cp -pr ${TEMP3}/* $RUTA_CACHE/
+	echo
+	borrar_temp
+	salir 1
+fi
+
+borrar_temp
+
+for line in $(cat $ARCHIVO_TOPICOS);do
+	busqueda="^$line	"
+	ruta_topico=$(egrep "$busqueda" $ARCHIVO_RUTAS_TOPICOS |cut -f 2)
+	find "$ruta_topico" -type f -iname "chuleta*.txt" \
 	|sed -r "s|$DIRBASE/$line||g" \
 	|sed -r "s|\.txt||g" \
 	|sed -r "s|chuleta_||g" \
@@ -18,7 +52,7 @@ for line in $(cat ~/.cache/chu/lista_topicos);do
 	|sed -r "s|_| |g" \
 	|tr ' ' '\n' \
 	|sort -u \
-	|tr '\n' ' ' >  ~/.cache/chu/lista_$line
+	|tr '\n' ' ' > $RUTA_CACHE/lista_$line
 done
 
 find "$DIRBASE" -type f -iname "chuleta*.txt" \
@@ -29,9 +63,9 @@ find "$DIRBASE" -type f -iname "chuleta*.txt" \
 |sed -r "s|_| |g" \
 |tr ' ' '\n' \
 |sort -u \
-|tr '\n' ' ' >  ~/.cache/chu/lista_comp
+|tr '\n' ' ' >  $RUTA_CACHE/lista_comp
 
-echo '	' >> ~/.cache/chu/lista_comp
+echo '	' >> $RUTA_CACHE/lista_comp
 
 exit 0
 
