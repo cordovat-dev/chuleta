25bb2988566e8ec4f02b242f6ab28c256347cdcd
cordovatj <cordovatj@pdvsa.com>
2019-11-20 10:29:58 -0400

    Creación de carpetas cache en deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,12 @@
 
 #sudo cp -f sbd.1 /usr/share/man/man1/
 #sudo gzip -f /usr/share/man/man1/sbd.1
+if [ ! -d "~/.cache/chu" ];then
+	mkdir ~/.cache/chu
+fi
+if [ ! -d "~/.cache/chu.logs" ];then
+	mkdir ~/.cache/chu.logs
+fi
 sudo cp -f chu.auto /etc/bash_completion.d/
+
 
