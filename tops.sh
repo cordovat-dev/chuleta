9547ae5c9c14906832753d3ab7e965e5273982be
cordovat <cordovat@gmail.com>
2021-05-30 14:19:20 +0200

    Several random admonition messages when using tops

--- tops.sh
+++ tops.sh
@@ -27,8 +27,18 @@ filter() {
 	done
 }
 
+arr[0]="You should consider trying to learn the following by heart:"
+arr[1]="It's about time you memorize the following:"
+arr[2]="Don't just copy and paste, understand!:"
+arr[3]="You use these ones frequently, try to learn them for once:"
+arr[4]="Try typing instead of copy+pasting, so you don't forget this:"
+arr[5]="Print these ones and pin them to your corkboard:"
+arr[6]="These ones should already have become second nature to you:"
+arr[7]="I guess you already know these topics without looking them up:"
+rand=$[$RANDOM % ${#arr[@]}]
+
 echo
-echo You should consider trying to learn the following by heart:
+echo ${arr[$rand]}
 echo
 sort $ARCHIVO | uniq -c | sort -nrk 1 > $TEMP1
 AVG=$(cat $TEMP1| awk '{print $1}' | avg)
