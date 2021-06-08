d0eb31e327361c5895cf13efafa3ffeba48bf842
cordovat <cordovat@gmail.com>
2021-06-08 14:10:11 +0200

    Changed bash functions to awk in tops.sh, 7x faster now

--- tops.sh
+++ tops.sh
@@ -5,28 +5,6 @@ RUTA=$(dirname $0)
 TEMP1=$(mktemp /tmp/chuleta.XXXXX)
 TEMP2=$(mktemp /tmp/chuleta.XXXXX)
 
-avg() {
-	local -i COUNT=0;
-	local -i SUM=0;
-	while read line
-	do
-		((COUNT ++))
-		((SUM = SUM + $line))
-	done;
-	AVG=$(( $SUM/$COUNT ))
-	echo $AVG	
-}
-
-filter() {
-	NUM=$1
-	while read line
-	do	
-		if [[ $(echo $line|awk '{print $1}') -gt $NUM ]];then
-			echo $line
-		fi
-	done
-}
-
 arr[0]="You should consider trying to learn the following by heart:"
 arr[1]="It's about time you memorize the following:"
 arr[2]="Don't just copy and paste, understand!:"
@@ -41,12 +19,11 @@ echo
 echo ${arr[$rand]}
 echo
 sort $ARCHIVO | uniq -c | sort -nrk 1 > $TEMP1
-AVG=$(cat $TEMP1| awk '{print $1}' | avg)
-filter $AVG < $TEMP1 > $TEMP2
-AVG=$(cat $TEMP2| awk '{print $1}' | avg)
-filter $AVG < $TEMP2
+AVG=$(awk '{acum = acum + $1} END {print acum/NR}' $TEMP1)
+awk -v AVG=$AVG '$1 >= AVG' < $TEMP1 > $TEMP2
+AVG=$(awk '{acum = acum + $1} END {print acum/NR}' $TEMP2)
+awk -v AVG=$AVG '$1 >= AVG {print $1,$2}' < $TEMP2
 rm "$TEMP1 $TEMP2" 2> /dev/null
-
 
 
 
