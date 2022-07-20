ccaa5d1a822d3e7dc7d4cf5ba4a8231a1deff11f
cordovat-dev <cordovat@gmail.com>
2022-07-20 17:54:25 +0200

    Parameterizatio of fmt2 and colorize main topic

--- fmt2.sh
+++ fmt2.sh
@@ -8,28 +8,53 @@ PARM2="$2" # change "chuletas" legend to "items"
 set -u
 RUTA=`dirname $0`
 COUNT=0
-COLOUR=1
+COLOUR=0
 COPEN=$(tput sgr0)
 CCLOSE=$(tput sgr0)
+CTOPEN=$(tput sgr0)
+REPORT=0
+LEGEND="chuletas"
+TITLE="Chuletas"
+
+while getopts rl:t:c flag
+do
+    case "${flag}" in
+        r) REPORT=1;;
+        l) LEGEND=${OPTARG};;
+		t) TITLE=${OPTARG};;
+        c) COLOUR=1;;
+    esac
+done
 
 if [ $COLOUR -eq 1 ] ;then
 	COPEN=$(tput setaf 3) # yellow
 	CCLOSE=$(tput sgr0)
+	CTOPEN=$(tput setaf 5)
 fi
 
+> /tmp/pruebacolor.txt
+printf "  %-4s%s\n" "#" $TITLE
+echo
 while read linea
 do
 	COUNT=$(( $COUNT + 1 ))
-	if [ "$PARM1" = "-n" ]; then
+	if [ $REPORT -eq 1 ]; then
 		printf "  %s\n" $linea
 	else
-		PRUEBA=$(printf "  %s%-4s%s%s\n" $COPEN $COUNT $CCLOSE $linea)
-		printf "  %s%-4s%s%s\n" $COPEN $COUNT $CCLOSE $linea
+		if [ $COLOUR -eq 1 ];then
+			MAIN_TOPIC=$(echo $linea | grep -Eo "^[^/]+")
+			REST_OF_PATH=$(echo $linea | grep -Eo "/.*$")
+			#PRUEBA=$(printf "  %s%-4s%s%s\n" $COPEN $COUNT $CCLOSE $linea)
+			printf "  %s%-4s%s%s%s%s\n" $COPEN $COUNT $CTOPEN $MAIN_TOPIC $CCLOSE $REST_OF_PATH >> /tmp/pruebacolor.txt
+		else
+			printf "  %-4s%s\n" $COUNT $linea >> /tmp/pruebacolor.txt
+		fi
 	fi
 done
-if [ "$PARM1" = "-n" ]; then
+cat /tmp/pruebacolor.txt
+if [ $REPORT -eq 1 ]; then
 	echo
-	echo "  Chuletas"
+	echo "  $LEGEND"
 fi
 #echo
 #echo $PRUEBA
