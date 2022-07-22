d3898782128d2e81cd37b9db41ab55094ff83dac
cordovat-dev <cordovat@gmail.com>
2022-07-22 20:42:24 +0200

    Fix: one off error printing separators

--- ct.sh
+++ ct.sh
@@ -12,17 +12,17 @@ DATA=$2
 
 IFS="/" read -a myarray <<< $DATA
 declare -i y=1
-printf "  %s%-4s%s%s/" $COPEN $COUNT $CTOPEN ${myarray[0]}
+printf "  %s%-4s%s%s" $COPEN $COUNT $CTOPEN ${myarray[0]}
 
 if [ ${#myarray[@]} -gt 2 ];then
-	printf "%s%s" $C2TOPEN ${myarray[1]}
+	printf "/%s%s" $C2TOPEN ${myarray[1]}
 	y=2
 	if [ ${#myarray[@]} -gt 3 ];then
 		printf "/%s%s" $C3TOPEN ${myarray[2]}
 		y=3
 	fi
-	printf "%s" $CCLOSE
 fi
+printf "%s" $CCLOSE
 
 for (( i=y; i<${#myarray[@]}; i++ ));
 do
