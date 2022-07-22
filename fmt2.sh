cordovat-dev/chuleta

commit df24f2f811efff808b12c2805690b6e9d7511171

Fix: err msg on deleting non existing menu cache file

main.sh
@@ -55,8 +55,8 @@ function menu {
 	COUNT=`wc -l < $1`
 	cat $1 >> "$TEMP"
 	colour=$(test $COLOUR = "YES" && echo "-c" && echo "")
-	rm ${MENUCACHE}
-	rm ${MENUCACHE_NC}
+	rm ${MENUCACHE} &> /dev/null
+	rm ${MENUCACHE_NC} &> /dev/null
 	$RUTA/./fmt2.sh $colour -f ${MENUCACHE_NC} < "$TEMP" | tee ${MENUCACHE}
 	
 	echo 

commit 33368cfbd0b2bb03fe0748796e78901dd30a761a

Fix: error introduced by df24f2f (stopping when menu)

fmt2.sh
@@ -42,11 +42,9 @@ fi
 		printf "  %-4s%s\n" "#" $TITLE
 		echo
 	fi
-	set -x
 	if [ -n "$NOCOLOR_FILE" ];then
 		echo -n "" > ${NOCOLOR_FILE}
 	fi
-	set +x
 while read linea
 do
 	COUNT=$(( $COUNT + 1 ))

main.sh
@@ -61,8 +61,8 @@ function menu {
 	COUNT=`wc -l < $1`
 	cat $1 >> "$TEMP"
 	colour=$(test $COLOUR = "YES" && echo "-c" && echo "")
-	rm ${MENUCACHE} &> /dev/null
-	rm ${MENUCACHE_NC} &> /dev/null
+	test -f ${MENUCACHE} && rm ${MENUCACHE}
+	test -f ${MENUCACHE_NC} && rm ${MENUCACHE_NC}
 	$RUTA/./fmt2.sh $colour -f ${MENUCACHE_NC} < "$TEMP" | tee ${MENUCACHE}
 	
 	echo 