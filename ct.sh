6d397b10b8e46c2bd103a2301aa1310331f9ca69
cordovat-dev <cordovat@gmail.com>
2022-07-23 14:30:16 +0200

    Refactored color variables

--- ct.sh
+++ ct.sh
@@ -2,11 +2,15 @@
 
 set -euo pipefail
 
-COPEN=$(tput setaf 3) # yellow
-CCLOSE=$(tput sgr0)
-CTOPEN=$(tput setaf 5) # magenta
-C2TOPEN=$(tput setaf 2) # green
-C3TOPEN=$(tput setaf 6) # cyan
+declare -A hues
+RUTA=`dirname $0`
+source $RUTA/colorsets.conf
+
+CNORMAL=${hues[NORMAL]}
+CINDEX=${hues[YELLOW]}
+C1TOPIC=${hues[GREEN]}
+C2TOPIC=${hues[MAGENTA]}
+C3TOPIC=${hues[CYAN]}
 COUNT=0
 DATA=""
 COLOUR=0
@@ -28,17 +32,17 @@ fi
 IFS="/" read -a myarray <<< $DATA
 
 declare -i y=1
-printf "  %s%-4s%s%s" $COPEN $COUNT $CTOPEN ${myarray[0]}
+printf "  %s%-4s%s%s" $CINDEX $COUNT ${C1TOPIC} ${myarray[0]}
 
 if [ ${#myarray[@]} -gt 2 ];then
-	printf "/%s%s" $C2TOPEN ${myarray[1]}
+	printf "/%s%s" ${C2TOPIC} ${myarray[1]}
 	y=2
 	if [ ${#myarray[@]} -gt 3 ];then
-		printf "/%s%s" $C3TOPEN ${myarray[2]}
+		printf "/%s%s" ${C3TOPIC} ${myarray[2]}
 		y=3
 	fi
 fi
-printf "%s" $CCLOSE
+printf "%s" $CNORMAL
 
 for (( i=y; i<${#myarray[@]}; i++ ));
 do
