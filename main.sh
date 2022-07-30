d44254eda53bc54635878a3565b8f1e05a75c57d
cordovat-dev <cordovat@gmail.com>
2022-07-30 16:23:15 +0200

    Force check age of locate db before using grl.sh

--- main.sh
+++ main.sh
@@ -158,6 +158,9 @@ elif [ "$TERMINO" = "--random" ];then
 	echo $CHULETA
 	$COMANDO $CHULETA "--random"
 else
+	set +e
+	locate $MAX_DB_AGE -A -d $RUTA_CACHE/db $RANDOM$RANDOM$RANDOM$RANDOM
+	set -e
 	cat $RUTA_CACHE/db.txt|$RUTA/grl.sh $LISTA_PALABRAS  > $TEMPORAL
 	# locate $MAX_DB_AGE -A -d $RUTA_CACHE/db -wr "chuleta_.*\.txt$" $LISTA_PALABRAS | sed -r "s|$BASE_DIR/||g" > $TEMPORAL
 fi
@@ -177,9 +180,3 @@ set +e
 find /tmp/chuleta.* -mtime +1 -delete &>/dev/null
 find $RUTA_CACHE -iname "menu*" -mmin +240 -delete &>/dev/null
 salir 0
-
-
-
-
-
-
