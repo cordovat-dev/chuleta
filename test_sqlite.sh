d0d4e5f8d49356528d5bc54b0451a35e5dba7d3c
cordovat-dev <cordovat@gmail.com>
2022-08-02 21:46:07 +0200

    Faster SQLite load test

--- test_sqlite.sh
+++ test_sqlite.sh
@@ -2,25 +2,26 @@
 
 set -euo pipefail
 
-TEMP=$(mktemp /tmp/chuleta.XXXXX)
-BASE_DIR=/c/Users/Global/chuleta/chuleta-data/
-DB=/c/Users/Global/chuleta/chuleta/chuletas.db
-SQLTEMP=$(mktemp /tmp/chuleta_insertsXXXXX.sql)
+BASE_DIR="/c/Users/Global/chuleta/chuleta-data/"
+DB="/c/Users/Global/chuleta/chuleta/chuletas.db"
+DATATEMP=$(mktemp /tmp/chuleta_insertsXXXXX)
+SCRIPTTEMP=$(mktemp /tmp/chuleta_insertsXXXXX)
 find $BASE_DIR -regextype sed \
 -regex "^.*[a-z0-9/_-]*chuleta_[a-z0-9/_-]*\.txt$"|\
-sed 's/$BASE_DIR//g' > $TEMP
-COUNT=0
-echo "delete from chuleta;" > $SQLTEMP
-while read linea
-do
-	# -vx
-	# sqlite3 $DB "insert into chuleta (path) values ('"$linea"');"
-	# +vx
-	COUNT=$(( $COUNT + 1 ))
-	echo "insert into chuleta (path) values ('"$linea"');" >> $SQLTEMP
-done < $TEMP
-set -x 
-set -v
-sqlite3 $DB ".read "$SQLTEMP
-rm $TEMP $SQLTEMP
+sed "s|$BASE_DIR||g" > $DATATEMP
+
+echo ".echo on" >> $SCRIPTTEMP
+echo "drop table if exists tempimp;" >> $SCRIPTTEMP
+echo "create temp table tempimp(path TEXT);" >> $SCRIPTTEMP
+echo "delete from chuleta;" >> $SCRIPTTEMP
+echo "select count(*) from chuleta;" >> $SCRIPTTEMP
+echo ".mode csv" >> $SCRIPTTEMP
+echo ".import $DATATEMP tempimp" >> $SCRIPTTEMP
+echo "insert into chuleta select null,path from tempimp;" >> $SCRIPTTEMP
+echo "select count(*) from chuleta;" >> $SCRIPTTEMP
+echo "select * from chuleta limit 5;" >> $SCRIPTTEMP
+echo ".quit" >> $SCRIPTTEMP
+# cat $SCRIPTTEMP
+sqlite3 $DB ".read "$SCRIPTTEMP
+rm $DATATEMP $SCRIPTTEMP
 
