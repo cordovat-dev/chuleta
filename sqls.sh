f27b251713a15646bb6474ed536057c891d25d1f
cordovat-dev <cordovat@gmail.com>
2022-08-05 17:16:13 +0200

    Echo off and better messages during db update

--- sqls.sh
+++ sqls.sh
@@ -25,19 +25,18 @@ find $BASE_DIR -regextype sed \
 -regex "^.*[.a-z0-9/_-]*chuleta_[.a-z0-9/_-]*\.txt$"|\
 sed "s|$BASE_DIR/||g" > $DATATEMP
 
-echo ".echo on" >> $SCRIPTTEMP
 echo "drop table if exists tempimp;" >> $SCRIPTTEMP
 echo "create temp table tempimp(path TEXT);" >> $SCRIPTTEMP
+echo ".mode line" >> $SCRIPTTEMP
+echo "select count(*) before from chuleta;" >> $SCRIPTTEMP
 echo "delete from chuleta;" >> $SCRIPTTEMP
-echo "select count(*) from chuleta;" >> $SCRIPTTEMP
 echo ".mode csv" >> $SCRIPTTEMP
 echo ".import $DATATEMP tempimp" >> $SCRIPTTEMP
-echo "select count(*) from tempimp;" >> $SCRIPTTEMP
+echo ".mode line" >> $SCRIPTTEMP
+echo "select count(*) processing from tempimp;" >> $SCRIPTTEMP
 echo "insert into chuleta select null,path from tempimp;" >> $SCRIPTTEMP
-echo "select count(*) from chuleta;" >> $SCRIPTTEMP
-echo "select * from chuleta limit 5;" >> $SCRIPTTEMP
+echo "select count(*) after from chuleta;" >> $SCRIPTTEMP
 echo ".quit" >> $SCRIPTTEMP
-# cat $SCRIPTTEMP
+
 sqlite3 $DB ".read "$SCRIPTTEMP
 rm $DATATEMP $SCRIPTTEMP
-
