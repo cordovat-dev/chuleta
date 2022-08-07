9daaea091ecc1229cd7a1dd531c8ea01e39c1ba5
cordovat-dev <cordovat@gmail.com>
2022-08-07 19:22:36 +0200

    Modify deploy to include SQLite databases

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 DIR=$(dirname "$0")
 MINGW=$([[ "$(uname -a)" =~ ^MINGW ]] && echo YES || echo NO)
+RUTA=$(dirname $0)
+BACKUPNAME=""
 
 if [ $MINGW == "YES" ];then
 	set +e
@@ -17,6 +19,20 @@ fi
 if [ ! -d ~/.cache/chu ];then
 	mkdir ~/.cache/chu
 fi
+
+if [ -f ~/.cache/chu/chuletas.db ];then
+	BACKUPNAME="$(echo ~/.cache/chu/chuletas.db.$(date +%Y%m%d%H%M%S))"
+	mv ~/.cache/chu/chuletas.db $BACKUPNAME
+	echo "Old chuletas.db moved to $BACKUPNAME"
+fi
+sqlite3 ~/.cache/chu/chuletas.db ".read "$RUTA/sqlite_db_schema.sql
+if [ -f ~/.cache/chu/frequent.db ];then
+	BACKUPNAME="$(echo ~/.cache/chu/frequent.db.$(date +%Y%m%d%H%M%S))"
+	mv ~/.cache/chu/frequent.db $BACKUPNAME
+	echo "Old frequent.db moved to $BACKUPNAME"
+fi
+sqlite3 ~/.cache/chu/frequent.db ".read "$RUTA/sqlite_frequent_db_schema.sql
+	
 if [ ! -d ~/.cache/chu.logs ];then
 	mkdir ~/.cache/chu.logs
 fi
@@ -26,6 +42,7 @@ fi
 if [ ! -f ~/.config/chu/chu.conf ];then
 	cp $DIR/chu.conf ~/.config/chu/
 	echo MINGW=$MINGW >> ~/.config/chu/chu.conf
+	echo "...Please edit ~/.config/chu/chu.conf file."
 fi
 
 if [ $MINGW == "YES" ];then
@@ -33,3 +50,5 @@ if [ $MINGW == "YES" ];then
 else
 	sudo cp -f $DIR/chu.auto /etc/bash_completion.d/
 fi
+
+echo "... Please run chu --update before using the utility."
