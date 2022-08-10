cordovat-dev/chuleta

commit 4f15b54b47090fc974dd227fc9dccdb6c0c63d55

SQLite search 'else' case

main.sh
@@ -170,7 +170,7 @@ else
 	set +e
 	test $NO_OLD_DB_WRN -eq 1 || locate $MAX_DB_AGE -A -d $RUTA_CACHE/db $RANDOM$RANDOM$RANDOM$RANDOM
 	set -e
-	cat $RUTA_CACHE/db.txt|$RUTA/grl.sh $LISTA_PALABRAS  > $TEMPORAL
+	sqlite3 $RUTA/chuletas.db "$($RUTA/gs.sh $@)" > $TEMPORAL
 fi
 
 CANT_RESULTADOS=`cat $TEMPORAL | wc -l`

gs.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+COUNT=0
+echo -n "select path from chuleta"
+for ARG in $*; do
+	COUNT=$(( $COUNT + 1 ))
+	if [ $COUNT -eq 1 ];then
+		echo -n " where "
+	fi
+	echo -n "path like '%$ARG%'"
+	if [ $COUNT -lt $# ]; then
+		echo -n " and "
+	fi
+done

commit 4bae5099a79eba29188d9229b146f05fe08deedf

Fix: --edit bug introduced in 4f15b54

main.sh
@@ -175,7 +175,7 @@ elif [ "$TERMINO" = "--random" ];then
 	$COMANDO $CHULETA "--random"
 else
 	$RUTA/co.sh -w $NO_OLD_DB_WRN -c $RUTA_CACHE
-	sqlite3 $RUTA_CACHE/chuletas.db "$($RUTA/gs.sh $@)" > $TEMPORAL
+	sqlite3 $RUTA_CACHE/chuletas.db "$($RUTA/gs.sh $LISTA_PALABRAS)" > $TEMPORAL
 fi
 
 CANT_RESULTADOS=`cat $TEMPORAL | wc -l`