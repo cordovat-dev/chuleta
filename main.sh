5bffd877a9964484a6577e7ec4a731e56b037c5c
cordovat-dev <cordovat@gmail.com>
2022-08-15 18:34:59 +0200

    Added --quick-update functionality

--- main.sh
+++ main.sh
@@ -91,7 +91,11 @@ function reporte {
 	echo
 }
 
-if [ "$TERMINO" = "--update" ];then
+function  update() {
+	local autocomp=""
+	set +u
+	autocomp="$1"
+	set -u
 	echo "Backing up database"
 	echo "Updating database"
 	cp "$RUTA_CACHE/db" "$RUTA_CACHE/db.$(date +%Y%m%d%H%M%S)"
@@ -101,10 +105,18 @@ if [ "$TERMINO" = "--update" ];then
 	$SUDO_COMMAND updatedb --localpaths="$BASE_DIR" --output="$RUTA_CACHE/db" --prunepaths="$BASE_DIR/.git"
 	locate $MAX_DB_AGE -A -d $RUTA_CACHE/db -wr "chuleta_.*\.txt$" | sed -r "s|$BASE_DIR/||g" > "$RUTA_CACHE/db.txt"
 	test -n "${MENUCACHE}" && test -f "${MENUCACHE}" && rm "${MENUCACHE}"
-	echo "Generating autocompletion"
-	$RUTA/gac.sh $BASE_DIR
+	if [ "$autocomp" != "quick" ];then
+		echo "Generating autocompletion"
+		$RUTA/gac.sh $BASE_DIR
+	fi
 	echo Done.
 	exit 0
+}
+
+if [ "$TERMINO" = "--update" ];then
+	update
+elif [ "$TERMINO" = "--quick-update" ];then	
+	update quick
 elif [ "$TERMINO" = "--totals" ];then
 	echo
 	for f in $(ls $BASE_DIR);do
