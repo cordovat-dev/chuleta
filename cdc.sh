9876677a1e85ab6b7e0a1c18fdad064e2e9b7077
cordovat-dev <cordovat@gmail.com>
2022-08-21 13:48:27 +0200

    Simpler check dead frequent with sed

--- cdc.sh
+++ cdc.sh
@@ -8,6 +8,7 @@ function exit_handler {
 	test -n "${TEMPSCRIPT}" && test -f "${TEMPSCRIPT}" && rm "${TEMPSCRIPT}"
 	test -n "${TEMPSED}" && test -f "${TEMPSED}" && rm "${TEMPSED}"
 	test -n "${TEMPRESULTDATA}" && test -f "${TEMPRESULTDATA}" && rm "${TEMPRESULTDATA}"
+	test -n "${TEMPRESULTDATA2}" && test -f "${TEMPRESULTDATA2}" && rm "${TEMPRESULTDATA2}"
 	exit $1
 }
 
@@ -16,31 +17,40 @@ RUTA_CACHE=~/.cache/chu
 BASE_DIR=~/chuleta/chuleta-data
 TEMPDATA=$(mktemp /tmp/chuleta.XXXXX)
 TEMPRESULTDATA=$(mktemp /tmp/chuleta.XXXXX)
+TEMPRESULTDATA2=$(mktemp /tmp/chuleta.XXXXX)
 TEMPSCRIPT=$(mktemp /tmp/chuleta.XXXXX)
 TEMPSED=$(mktemp /tmp/chuleta.XXXXX)
 EXCODE=0
 BACKUPTABLE="frequent_log_$(date +%Y%m%d%H%M%S)"
+
+cp -p $RUTA_CACHE/frequent.db $RUTA_CACHE/frequent.db.$(date +%Y%m%d%H%M%S)
+
+sqlite3 $RUTA_CACHE/frequent.db <<EOF
+.echo on
+select 'Exporting '||(select count(*) from frequent_log)||' rows';
+.mode csv
+.echo off
+.output ${TEMPDATA}
+select * from frequent_log;
+EOF
+
 cd $BASE_DIR
 
-echo ".echo on" > "${TEMPSCRIPT}"
-echo "select 'Exporting '||(select count(*) from frequent_log)||' rows';" >> "${TEMPSCRIPT}"
-echo ".mode csv" >> "${TEMPSCRIPT}"
-echo ".separator ' '" >> "${TEMPSCRIPT}"
-echo ".output $TEMPDATA " >> "${TEMPSCRIPT}"
-echo ".output $TEMPDATA " >> "${TEMPSCRIPT}"
-sqlite3 $RUTA_CACHE/frequent.db ".read "${TEMPSCRIPT}
+git diff --name-status -C $(git rev-list HEAD|tail -1)..HEAD|grep "^R"|awk '{printf("s#%s#%s#g\n",$2,$3)}' > ${TEMPSED}
+sed -Ef ${TEMPSED} ${TEMPDATA} > ${TEMPRESULTDATA}
+git diff --name-status -C $(git rev-list HEAD|tail -1)..HEAD|grep "^D"|awk '{printf("#%s#d\n",$2)}' > ${TEMPSED}
+sed -Ef ${TEMPSED} ${TEMPRESULTDATA} > ${TEMPRESULTDATA2}
 
-cd $BASE_DIR
+sqlite3 $RUTA_CACHE/frequent.db <<EOF
+.echo on
+create table ${BACKUPTABLE} as select * from frequent_log;
+insert into ${BACKUPTABLE} select * from frequent_log;
+delete from frequent_log;
+.mode csv
+.import ${TEMPRESULTDATA2} frequent_log
+select 'Imported '||(select count(*) from frequent_log)||' rows';
+.exit
+EOF
 
-git diff --name-status -C $(git rev-list HEAD|tail -1)..HEAD|grep "^R"|awk '{printf("s#%s#%s#g\n",$2,$3)}' > $TEMPSED
-sed -Ef $TEMPSED $TEMPDATA > $TEMPRESULTDATA
-
-echo ".echo on" > "${TEMPSCRIPT}"
-echo "create table $BACKUPTABLE as select * from frequent_log;" >> "${TEMPSCRIPT}"
-echo "insert into $BACKUPTABLE select * from frequent_log;" >> "${TEMPSCRIPT}"
-echo "delete from frequent_log;" >> "${TEMPSCRIPT}"
-echo ".mode csv" >> "${TEMPSCRIPT}" >> "${TEMPSCRIPT}"
-echo ".separator ' '" >> "${TEMPSCRIPT}" >> "${TEMPSCRIPT}"
-echo ".import $TEMPRESULTDATA frequent_log"
-echo ".exit"
+exit 0
 
