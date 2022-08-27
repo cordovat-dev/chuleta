a6471b0696d4cca87f0e7c637736ab93df149290
cordovat-dev <cordovat@gmail.com>
2022-08-27 19:58:18 +0200

    Fix: now the frequent report prints in one go

--- tops.sh
+++ tops.sh
@@ -4,8 +4,7 @@ trap exit_handler EXIT
 
 function exit_handler {
 	set +u
-	test -n "${TEMP1}" && test -f "${TEMP1}" && rm "${TEMP1}"
-	test -n "${TEMP2}" && test -f "${TEMP2}" && rm "${TEMP2}"
+	test -n "${TEMP}" && test -f "${TEMP}" && rm "${TEMP}"
 	exit $1
 }
 
@@ -13,6 +12,7 @@ set -euo pipefail
 
 ARCHIVO=""
 RUTA=$(dirname $0)
+TEMP=$(mktemp /tmp/chuleta.XXXXX)
 
 COLOUR=0
 
@@ -30,14 +30,20 @@ arr[0]="You should consider trying to learn the following by heart:"
 arr[1]="It's about time you memorize the following:"
 arr[2]="Don't just copy and paste, understand!:"
 arr[3]="You use these ones frequently, try to learn them for once:"
-arr[4]="Try typing instead of copy+pasting, so you don't forget this:"
+arr[4]="Try typing instead of copy+pasting, so you don't forget these:"
 arr[5]="Print these ones and pin them to your corkboard:"
 arr[6]="These ones should already have become second nature to you:"
 arr[7]="I guess you already know these topics without looking them up:"
+arr[8]="Do these ones ring a bell?:"
+arr[9]="You will have to use some mnemonic to learn some of these:"
+arr[10]="You should know better:"
 rand=$[$RANDOM % ${#arr[@]}]
 
-echo
-echo ${arr[$rand]}
-echo
+cat <<EOF > $TEMP
 
-$RUTA/./fmt2.sh $(test $COLOUR = 1 && echo "-c" || echo "") -n < $ARCHIVO
+${arr[$rand]}
+
+EOF
+
+$RUTA/./fmt2.sh $(test $COLOUR = 1 && echo "-c" || echo "") -n < $ARCHIVO >> $TEMP
+cat $TEMP
