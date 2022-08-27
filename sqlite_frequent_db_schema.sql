69a376c06bec1a2f774c437a31b68c4c72080b44
cordovat-dev <cordovat@gmail.com>
2022-08-27 20:46:36 +0200

    Deleted no longer needed objects from frequent db schema

--- sqlite_frequent_db_schema.sql
+++ sqlite_frequent_db_schema.sql
@@ -1,21 +1,3 @@
-CREATE TABLE frequent(
-        path text,
-        count integer
-);
-CREATE VIEW v_tops as
-select count, path
-from
-	frequent
-where count > (
-	select
-		avg(count)
-	from
-		frequent
-	where
-		count > (select avg(count) from frequent)
-) order by count desc
-/* v_tops(count,path) */
-/* v_tops(count,path) */;
 CREATE TABLE frequent_log(path text, count integer);
 CREATE VIEW v_log_count as select path, count(*) count from frequent_log group by path
 /* v_log_count(path,count) */;
