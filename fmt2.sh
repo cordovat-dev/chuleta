cordovat-dev/chuleta

commit ec10c80bce49ef5fcac6a3bce633e00959294773

Faster coloring output with sed. 1st version

fmt2.sh
@@ -20,6 +20,7 @@ TITLE="Chuletas"
 NOCOLOR_FILE=""
 HASNUMBERS=0
 HEADERCOL1="#"
+COUNT=0
 TEMP=$(mktemp /tmp/chuleta.XXXXX)
 
 while getopts rl:t:cnf: flag
@@ -47,27 +48,18 @@ fi
 	
 while read linea
 do
-	if [ $HASNUMBERS -eq 1 ];then
-		lineatemp=$linea
-		COUNT=$(awk '{print $1}' <(echo $lineatemp))
-		linea=$(awk '{print $2}' <(echo $lineatemp))
-	else
-		COUNT=$(( $COUNT + 1 ))
-	fi
+	COUNT=$(( $COUNT + 1 ))
 	if [ $REPORT -eq 1 ]; then
 		printf "  %s\n" $linea
 	else
-		if [ $COLOUR -eq 1 ];then
-			$RUTA/ct.sh -n $COUNT -d $linea -c >> $TEMP
+		if [ $HASNUMBERS -ne 1 ];then
+			printf "  %-4s%s\n" $COUNT $linea
 		else
-			printf "  %-4s%s\n" $COUNT $linea >> $TEMP
-		fi
-		if [ -n "$NOCOLOR_FILE" ];then
-			printf "  %-4s%s\n" $COUNT $linea >> ${NOCOLOR_FILE}
-		fi;
+			printf "  %s\n" $linea
+		fi	
 	fi
-done
-cat $TEMP
+done > $TEMP
+$RUTA/ac.sed $TEMP
 if [ $REPORT -eq 1 ]; then
 	echo
 	echo "  $COUNT $LEGEND"

ac.sed
@@ -1,6 +1,6 @@
 #!/bin/sed -Ef
 
-s#^([0-9]+ )([^/]*)/#\1[\2]/#g
+s#^( +)([0-9]+ )([^/]*)/#\1\2[\3]/#g
 s#]/([^/]*)/#]/(\1)/#g
 s#\(/([^/()]*)/#(/<\1>/#g
 s#\)/([^/()]*)/#)/<\1>/#g
@@ -17,3 +17,8 @@ s/>/(B[m/g
 
 s/\{/[31m/g
 s/\}/(B[m/g
+
+s/^( +[0-9]+) /(\1) /g
+
+s/^\(/[33m/g
+s/\)/(B[m/g

ct.sh
@@ -2,16 +2,8 @@
 
 set -euo pipefail
 
-declare -A hues
 RUTA=`dirname $0`
-source $RUTA/colorsets.conf
 
-CNORMAL=${hues[NORMAL]}
-CINDEX=${hues[YELLOW]}
-C1TOPIC=${hues[GREEN]}
-C2TOPIC=${hues[MAGENTA]}
-C3TOPIC=${hues[CYAN]}
-C4TOPIC=${hues[RED]}
 COUNT=0
 DATA=""
 COLOUR=0
@@ -30,33 +22,6 @@ if [ $COLOUR -eq 0 ];then
 	exit 0
 fi
 
-IFS="/" read -a myarray <<< $DATA
+echo "$COUNT $DATA" | $RUTA/ac.sed
 
-declare -i y=1
-printf "  %s%-4s%s%s" $CINDEX $COUNT ${C1TOPIC} ${myarray[0]}
-
-if [ ${#myarray[@]} -gt 2 ];then
-	printf "/%s%s" ${C2TOPIC} ${myarray[1]}
-	y=2
-fi
-
-if [ ${#myarray[@]} -gt 3 ];then
-	printf "/%s%s" ${C3TOPIC} ${myarray[2]}
-	y=3
-fi
-
-if [ ${#myarray[@]} -gt 4 ];then
-	printf "/%s%s" ${C4TOPIC} ${myarray[3]}
-	y=4
-fi
-
-printf "%s" $CNORMAL
-
-for (( i=y; i<${#myarray[@]}; i++ ));
-do
-	if [ $i -lt ${#myarray[@]} ];then
-		echo -n  "/"
-	fi;
-    printf "%s" ${myarray[$i]}
-done
-echo ""
+exit 0

commit 9099975c3609fbf466581766773edd588f6b7d53

Fix: bug in --cached introduced in ec10c80

fmt2.sh
@@ -67,6 +67,11 @@ if [ $COLOUR -eq 1 ]; then
 else
 	cat $TEMP
 fi
+
+if [ -n "$NOCOLOR_FILE" ];then
+	cp $TEMP ${NOCOLOR_FILE}
+fi;
+
 if [ $REPORT -eq 1 ]; then
 	echo
 	echo "  $COUNT $LEGEND"