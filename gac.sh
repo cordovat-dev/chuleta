1ae6be1bf60a59a461b61610d398932b91d69e39
cordovat-dev <cordovat@gmail.com>
2022-08-28 16:55:13 +0200

    Fix: autocomp won't hint suptopics as a term of father topic. See more.
    - Also a subtopic's terms were being excluded from its parents terms
    - Now all of a topic's content is considered in its autocompletion,
      including subtopic names and all the terms under subtopics
    - Legibity of the gac.sh was improved with descriptive names of temporary files

--- gac.sh
+++ gac.sh
@@ -4,9 +4,9 @@ trap exit_handler EXIT
 
 function exit_handler {
 	set +u
-	test -n "${TEMP}" && test -f "${TEMP}" && rm "${TEMP}"
-	test -n "${TEMP2}" && test -f "${TEMP2}" && rm "${TEMP2}"
-	test -n "${TEMP3}" && [[ "${TEMP3}" =~ ^\/tmp\/.* ]] && test -d "${TEMP3}" && rm -rf "${TEMP3}"
+	test -n "${TEMP_TOPICOS}" && test -f "${TEMP_TOPICOS}" && rm "${TEMP_TOPICOS}"
+	test -n "${TEMP_RUTAS_TOPICOS}" && test -f "${TEMP_RUTAS_TOPICOS}" && rm "${TEMP_RUTAS_TOPICOS}"
+	test -n "${TEMP_BACKUP}" && [[ "${TEMP_BACKUP}" =~ ^\/tmp\/.* ]] && test -d "${TEMP_BACKUP}" && rm -rf "${TEMP_BACKUP}"
 	exit $1
 }
 
@@ -25,9 +25,9 @@ ARCHIVO_TOPICOS=$RUTA_CACHE/lista_topicos
 ARCHIVO_RUTAS_TOPICOS=$RUTA_CACHE/lista_rutas_topicos
 ARCHIVO_TOPICOS_REPETIDOS=$RUTA_CACHE/lista_topicos_repetidos
 ARCHIVO_LISTA_COMPLETA=$RUTA_CACHE/lista_comp
-TEMP=$(mktemp /tmp/chuleta.XXXXX)
-TEMP2=$(mktemp /tmp/chuleta.XXXXX)
-TEMP3=$(mktemp -d /tmp/chuleta.XXXXX)
+TEMP_TOPICOS=$(mktemp /tmp/chuleta.XXXXX)
+TEMP_RUTAS_TOPICOS=$(mktemp /tmp/chuleta.XXXXX)
+TEMP_BACKUP=$(mktemp -d /tmp/chuleta.XXXXX)
 
 # 1. searches all chuletas in the db,
 # 2. removes the basename,
@@ -39,27 +39,27 @@ sqlite3 "${CHULETADB}" "select path from v_chuleta_ap;"|\
 grep -o "^/.*\/"|\
 sed 's/.$//g'|\
 grep -o '[^/]*$'|\
-sort -u > $TEMP
+sort -u > $TEMP_TOPICOS
 
 # 1. searches all chuletas in the db,
 # 2. removes the basename,
 # 3. removes the trailing slash,
 # 4. creates a sorted unique list
-# 5. splits using slash and prints number of fields and all fields
+# 5. splits using slash and prints the last field, then all fields
 # 6. creates a sorted unique list
-# RESULT: a list of folders names (a folder for each topic/subtopic)
+# RESULT: a list of topics/suptopics and their corresponding folder
 sqlite3 "${CHULETADB}" "select path from v_chuleta_ap;"|\
 grep -o "^/.*/"|\
 sed -r 's#/$##g'|\
 sort -u|\
 awk 'BEGIN {FS="/"; OFS="\t"}{print $NF, $0}'|\
-sort -u > $TEMP2
+sort -u > $TEMP_RUTAS_TOPICOS
 
-cp -pr $RUTA_CACHE/* ${TEMP3}/
+cp -pr $RUTA_CACHE/* ${TEMP_BACKUP}/
 rm $RUTA_CACHE/*
 
-cp $TEMP $ARCHIVO_TOPICOS
-cp ${TEMP2} $ARCHIVO_RUTAS_TOPICOS
+cp $TEMP_TOPICOS $ARCHIVO_TOPICOS
+cp ${TEMP_RUTAS_TOPICOS} $ARCHIVO_RUTAS_TOPICOS
 
 if [ $(cat $ARCHIVO_RUTAS_TOPICOS |cut -f 1|uniq -c|grep -vn "1"|wc -l) -gt 0 ];then
 	echo
@@ -67,17 +67,17 @@ if [ $(cat $ARCHIVO_RUTAS_TOPICOS |cut -f 1|uniq -c|grep -vn "1"|wc -l) -gt 0 ];
 	echo
 	x="$(cat $ARCHIVO_RUTAS_TOPICOS |cut -f 1|uniq -c|grep -v "1"|awk '{print $2}')"
 	find $BASE_DIR -type d -iname "$x"
-	cp -pr ${TEMP3}/* $RUTA_CACHE/
+	cp -pr ${TEMP_BACKUP}/* $RUTA_CACHE/
 	echo
 	salir 1
 else
-	find ${TEMP3}/ -type f ! -iname "lista_*" -exec cp -pr {} $RUTA_CACHE/ \;
+	find ${TEMP_BACKUP}/ -type f ! -iname "lista_*" -exec cp -pr {} $RUTA_CACHE/ \;
 fi
 
 for line in $(cat $ARCHIVO_TOPICOS);do
 	busqueda="^$line	"
 	ruta_topico=$(egrep "$busqueda" $ARCHIVO_RUTAS_TOPICOS |cut -f 2)
-	sqlite3 "${CHULETADB}" "select path from v_chuleta_ap where path like '$ruta_topico/chuleta_%';"|\
+	sqlite3 "${CHULETADB}" "select path from v_chuleta_ap where path like '$ruta_topico/%chuleta_%';"|\
 	awk -v RTO="$ruta_topico" -f $RUTA_SCRIPT/glst.awk > $RUTA_CACHE/lista_$line
 done
 
