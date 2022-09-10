cordovat-dev/chuleta

commit 2116f6e413ac64597efd5281d49576a536f0a477

Added dependency check

deploy.sh
@@ -12,6 +12,22 @@ CONFIG_FILE=$RUTA_CONF/chu.conf
 CHULETADB=$RUTA_CACHE/chuletas.db
 FREQUENTDB=$RUTA_CACHE/frequent.db
 
+if ! command -v ssqlite3 &> /dev/null; then
+cat <<EOF
+
+Sqlite3 could not be found
+Please install sqlite3 in order to install
+and use Chuleta.
+EOF
+    exit 1
+fi
+
+if [   || echo <<EOF
+	sqlite3 is not installed in this system.
+	Please install sqlite3 to be able to installed
+	and use Chuleta
+EOF
+
 if [ $MINGW == "YES" ];then
 	set +e
 	net session > /dev/null 2>&1

commit d2428528c59b7f8347f0460b540ee99c82cb455e

Fix: bug in deploy introduced in 2116f6e

deploy.sh
@@ -12,7 +12,7 @@ CONFIG_FILE=$RUTA_CONF/chu.conf
 CHULETADB=$RUTA_CACHE/chuletas.db
 FREQUENTDB=$RUTA_CACHE/frequent.db
 
-if ! command -v ssqlite3 &> /dev/null; then
+if ! command -v sqlite3 &> /dev/null; then
 cat <<EOF
 
 Sqlite3 could not be found
@@ -21,12 +21,6 @@ and use Chuleta.
 EOF
     exit 1
 fi
-
-if [   || echo <<EOF
-	sqlite3 is not installed in this system.
-	Please install sqlite3 to be able to installed
-	and use Chuleta
-EOF
 
 if [ $MINGW == "YES" ];then
 	set +e