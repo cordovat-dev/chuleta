e6dd5ab663570b734baf84716e3b60e9a2484ccc
cordovat-dev <cordovat@gmail.com>
2022-11-29 10:14:40 +0100

    Also delete others Git-related database-scoped settings. Complements 7d2da4f.

--- udbs.sh
+++ udbs.sh
@@ -29,24 +29,24 @@ TEMP="$(mktemp /tmp/chuleta.XXXXX)"
 function generateDML {
 echo ".echo on"
 echo "BEGIN TRANSACTION;"
-# these ones are deleted to prevent gaps in numbers
+# these ones are deleted to PREVENT GAPS IN NUMBERS
 echo "delete from settings where key like 'GIT_REPO%';"
 echo "delete from settings where key like 'PREF_GIT_REPO2%';"
 source ${configfile}
 if [ "${GIT_INTEGRATION:-NO}" != "YES" ];then
        echo "delete from settings where key = 'LAST_GIT_TAG';"
-fi
+else
 awk -F= -f <(cat - <<-"EOF"
 	/GIT_REPO[0-9]+/ {
 		printf ("insert or replace into settings (key,value) values (\x27%s\x27,\x27%s\x27);\n",$1,$2) 
 	}
-	/NUM_DAYS_OLD/ {
-		printf ("insert or replace into settings (key,value) values (\x27%s\x27,\x27%s\x27);\n",$1,$2) 
-	}
 EOF
 )
+fi
+
 local bdir=$(echo ${BASE_DIR})
 echo "insert or replace into settings (key,value) values ('BASE_DIR','${bdir}');"
+echo "insert or replace into settings (key,value) values ('NUM_DAYS_OLD','${NUM_DAYS_OLD:-8}');"
 echo "END TRANSACTION;"
 }
 
