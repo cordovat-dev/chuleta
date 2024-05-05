29bda8d6284a85efd959f824119e0cb9cd06e9f6
cordovat-dev <cordovat@gmail.com>
2024-05-05 15:02:56 +0200

    Correct hard text when attaching fts db in gs

--- gs.sh
+++ gs.sh
@@ -7,8 +7,13 @@ if [ $# -eq 0 ]; then
 	exit 0
 fi
 
+CACHE_DIR=~/.cache/chu
+FTSDB="${CACHE_DIR}/chuletas_fts.db"
+
 COUNT=0
-echo -n "attach '/home/cordovat/.cache/chu/chuletas_fts.db' as ftsdb;"
+echo -n "attach '"
+echo -n ${FTSDB}
+echo -n "' as ftsdb;"
 echo -n "select path from chuleta_fts where chuleta_fts match '"
 for ARG in $*; do
 	COUNT=$(( ${COUNT} + 1 ))
